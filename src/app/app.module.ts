59d42a3be526138ac885294900660cf598db4503
angelapriyadharshini <angela.amarapala@gmail.com>
2019-11-18 00:14:17 +0530

    Create drag-drop component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DragAndDropComponent } from './components/drag-and-drop/drag-and-drop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragAndDropComponent
   ],
   imports: [
     BrowserModule
