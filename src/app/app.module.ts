bc6b6f1baccceed47e3908c453cbfe8e766e8570
angelapriyadharshini <angela.amarapala@gmail.com>
2019-12-14 02:50:28 +0530

    Added service providers to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DragAndDropComponent } from './components/drag-and-drop/drag-and-drop.component';
+import { DataService } from './services/data-service';
+import { ItemBoxService } from './services/itembox-service';
+import { ItemboxComponent } from './components/itembox/itembox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DragAndDropComponent
+    DragAndDropComponent,
+    ItemboxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataService, ItemBoxService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
